b55ae12931a97d8f64543b46a1617b4f2f7f5458
dongfangzhou1108 <18746061711@163.com>
2023-10-08 17:10:09 +0800

    note #pragma comment(lib, "event.lib")

--- evpp/windows_port.h
+++ evpp/windows_port.h
@@ -1,3 +1,11 @@
+/*
+ * @Author: dongfangzhou1108 18746061711@163.com
+ * @Date: 2023-10-08 16:48:49
+ * @LastEditors: dongfangzhou1108 18746061711@163.com
+ * @LastEditTime: 2023-10-08 17:10:03
+ * @FilePath: \evpp-0.7.0\evpp\windows_port.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #pragma once
 
 // Fix VS compile warning
@@ -68,7 +76,7 @@
 #endif
 #pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "glog.lib")
-#pragma comment(lib, "event.lib")
+// #pragma comment(lib, "event.lib")
 #ifndef H_LIBEVENT_VERSION_14
 #pragma comment(lib, "event_core.lib") // libevent2.0
 #pragma comment(lib, "event_extra.lib") // libevent2.0
